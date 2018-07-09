fbbdf1a116d9e3154c6ace99c56b3121a331c0e5
Francisco Oliveira <delley.fx@gmail.com>
2018-07-09 16:06:01 -0300

    Add ini config file

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module github.com/delley/gotiny
+
+require github.com/joho/godotenv v1.2.0
--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"flag"
+	"os"
+
+	"mod/github.com/joho/godotenv@v1.2.0"
 
 	"github.com/delley/gotiny/app"
 )
@@ -11,6 +14,15 @@ var port string
 func init() {
 	flag.StringVar(&port, "port", "8000", "Assigning the port that server should listen on.")
 	flag.Parse()
+
+	if err := godotenv.Load("config.ini"); err != nil {
+		panic(err)
+	}
+
+	envPort := os.Getenv("PORT")
+	if len(envPort) > 0 {
+		port = envPort
+	}
 }
 func main() {
 	s := app.NewServer()
